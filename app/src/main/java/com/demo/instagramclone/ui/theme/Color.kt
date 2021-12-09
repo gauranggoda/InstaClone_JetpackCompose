2f13260bffbcdb1164907c973e5b267c8fc22c71
Gaurang Goda <gaurang.goda@gmail.com>
2021-12-10 02:35:00 +0530

    Basic Theme Setup and changes

--- app/src/main/java/com/demo/instagramclone/ui/theme/Color.kt
+++ app/src/main/java/com/demo/instagramclone/ui/theme/Color.kt
@@ -2,6 +2,14 @@ package com.demo.instagramclone.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+val WhiteBackground = Color(0xFFFFFFFF)
+val TextBlack = Color(0xFF303030)
+val TextGrey = Color(0xFFEFEFEF)
+val TextLightBlue = Color(0xFF84DFFF)
+val TextDarkBlue = Color(0xFF000957)
+val ButtonBlue = Color(0xFF84DFFF)
+val BlueAccent = Color(0xFF84DFFF)
+
 val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
